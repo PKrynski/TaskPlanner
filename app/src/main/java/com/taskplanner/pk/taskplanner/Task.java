53ebbd48e32143f696bce6ee2af1b2a9a4c393c4
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-10-08 21:10:02 +0200

    Task class created.

---
+++ app/src/main/java/com/taskplanner/pk/taskplanner/Task.java
@@ -0,0 +1,36 @@
+package com.taskplanner.pk.taskplanner;
+
+public class Task {
+
+    private String name;
+    private String description;
+    private String category;
+    private boolean completed;
+
+    public Task(String name, String description, String category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.completed = false;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
