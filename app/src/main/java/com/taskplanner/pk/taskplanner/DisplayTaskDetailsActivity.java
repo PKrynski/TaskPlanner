e0c173680ad12e4bfb752548e97ae26f649ce2f1
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-10-10 15:13:52 +0200

    New activity to display task details.

---
+++ app/src/main/java/com/taskplanner/pk/taskplanner/DisplayTaskDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.taskplanner.pk.taskplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayTaskDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_task_details);
+    }
+}
