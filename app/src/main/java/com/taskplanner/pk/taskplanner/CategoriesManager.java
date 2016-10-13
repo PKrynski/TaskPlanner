fc3ef5bb4e4852a9af741bc421fc6fce99a3fff7
Pawel Krynski <krynskip@ee.pw.edu.pl>
2016-10-13 21:50:24 +0200

    Extracted Category class.

--- app/src/main/java/com/taskplanner/pk/taskplanner/CategoriesManager.java
+++ app/src/main/java/com/taskplanner/pk/taskplanner/CategoriesManager.java
@@ -44,25 +44,4 @@ public class CategoriesManager {
         addNewCategory("Travel","purple");
     }
 
-
-
-
-    private static class Category {
-
-        private String name;
-        private String color;
-
-        public Category(String name, String color) {
-            this.name = name;
-            this.color = color;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getColor() {
-            return color;
-        }
-    }
 }
---
+++ app/src/main/java/com/taskplanner/pk/taskplanner/Category.java
@@ -0,0 +1,20 @@
+package com.taskplanner.pk.taskplanner;
+
+public class Category {
+
+    private String name;
+    private String color;
+
+    public Category(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
